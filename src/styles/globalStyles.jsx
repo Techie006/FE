61fe9e9c0d0bd3df1e76345528ef9d874e7ee2a9
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-30 16:40:16 +0900

    feat: add global style

---
+++ src/styles/globalStyles.jsx
@@ -0,0 +1,37 @@
+// TODO design 후 변경
+export const defaultTheme = {
+  // background
+  backgroundColor: "#FFFFFF",
+
+  // text
+  textColor: "#181B2E",
+  helperTextColor: "#bcbdc3",
+  errorTextColor: "#FD2053",
+  successTextColor: "#00BA00",
+
+  // link
+  mediumLinkColor: "#5732fb",
+  smallLinkColor: "gray",
+
+  // divider
+  dividerColor: "#FFFFFF",
+
+  // button
+  buttonColor: "#EFEFEF",
+  buttonTextColor: "#17192C",
+  buttonRadius: "10px",
+  iconColor: "#B7BBC7",
+
+  // accent color for hover effect
+  hoverButtonColor: "#5732fb",
+  hoverButtonTextColor: "#FFFFFF",
+  hoverIconColor: "#5732fb",
+  hoverTransform: "scale(1,1)",
+
+  // box-shadow
+  boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+  boxRadius: "5px",
+
+  // transition
+  transition: "all 0.3s",
+};
