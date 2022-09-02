28490830b48abffd67a2751171bb21f1e48f1d9e
ericagong <48196721+ericagong@users.noreply.github.com>
2022-09-02 20:02:57 +0900

    feat: recipe modal

---
+++ src/components/recipes/RecipesLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Recipes from "./Recipes";
+import styled from "styled-components";
+
+const RecipesLayout = () => {
+  return (
+    <StyledReipesWrapper>
+      <Recipes />
+    </StyledReipesWrapper>
+  );
+};
+
+export default RecipesLayout;
+
+const StyledReipesWrapper = styled.div`
+  width: 95%;
+`;
