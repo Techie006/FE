de87b59bcc630995ba8f085ec525ebedb3abb191
ericagong <48196721+ericagong@users.noreply.github.com>
2022-09-03 10:31:52 +0900

    feat: MOCK APIs for recipes/statistics

--- src/server/response_chae.jsx
+++ src/server/response_chae.jsx
@@ -1,28 +1,264 @@
 // MOCK API before connecting real API
 
 const RESP_CHAE = {
+  // statistics: 5 apis
   RECIPES: {
-    // 1. get ingredients state (/api/statistics/state)
-    GET_STATE_SUCCESS: {
+    // 1. get 5 recipes in certain page (/api/reipes?pageNum={pageNum}*pageLimit=&{pageLimit})
+    GET_RECIPES_SUCCESS: {
       result: true,
       content: {
-        // in-hurry, warning, fine
-        percentage: [0.3333, 0.3333, 0.3333],
-        count: [3, 3, 3],
+        current_page_num: 1,
+        total_page_num: 3,
+        recipes: [
+          {
+            id: 1,
+            recipe_name: "칼륨듬뿍고구마죽",
+            ingredients: ["고구마죽 고구마", "설탕", "찹쌀가루"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00017_2.png",
+            method: "끓이기",
+            category: "후식",
+            calorie: 200,
+          },
+          {
+            id: 2,
+            recipe_name: "누룽지 두부 계란죽",
+            ingredients: ["애호박", "표고버섯", "당근"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00016_2.png",
+            method: "끓이기",
+            category: "밥",
+            calorie: 380,
+          },
+          {
+            id: 3,
+            recipe_name: "새우두부계란찜",
+            ingredients: ["초밥 밥", "배합초 식초", "설탕"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00009_2.png",
+            method: "찌기",
+            category: "반찬",
+            calorie: 200,
+          },
+          {
+            id: 4,
+            recipe_name: "두부 곤약 나물 비빔밥",
+            ingredients: ["두부", "흰쌀", "현미쌀"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00010_2.png",
+            method: "끓이기",
+            category: "밥",
+            calorie: 200,
+          },
+          {
+            id: 5,
+            recipe_name: "저염 간장을 이용한 닭개장 비빔밥",
+            ingredients: ["흑미밥 쌀", "검은 쌀", "닭가슴살"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00011_2.png",
+            method: "끓이기",
+            category: "밥",
+            calorie: 200,
+          },
+        ],
       },
       status: {
         code: 200,
-        message: "식재료 상태 제공에 성공하였습니다.",
+        message: "해당 페이지 레시피 제공에 성공하였습니다.",
       },
     },
-    GET_STATE_FAIL: {
+    GET_RECIPES_LAST_SUCCESS: {
+      result: true,
+      content: {
+        current_page_num: 3,
+        total_page_num: 3,
+        recipes: [
+          {
+            id: 11,
+            recipe_name: "된장국",
+            ingredients: ["된장국 두부", "애느타리버섯", "감자"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00037_2.png",
+            method: "끓이기",
+            category: "국&찌개",
+            calorie: 260,
+          },
+        ],
+      },
+      status: {
+        code: 200,
+        message: "해당 페이지 레시피 제공에 성공하였습니다.",
+      },
+    },
+    // 2. get 5 recipes which are matching to keyworkd in certain page (/api/reipes/search)
+    GET_SEARCHED_RECIPES_SUCCESS: {
+      result: true,
+      content: {
+        current_page_num: 1,
+        total_page_num: 3,
+        recipes: [
+          {
+            id: 1,
+            recipe_name: "칼륨듬뿍고구마죽",
+            ingredients: ["고구마죽 고구마", "설탕", "찹쌀가루"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00017_2.png",
+            method: "끓이기",
+            category: "후식",
+            calorie: 200,
+          },
+          {
+            id: 2,
+            recipe_name: "누룽지 고구마 계란죽",
+            ingredients: ["애호박", "표고버섯", "당근"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00016_2.png",
+            method: "끓이기",
+            category: "밥",
+            calorie: 380,
+          },
+          {
+            id: 3,
+            recipe_name: "새우두부고구마계란찜",
+            ingredients: ["초밥 밥", "배합초 식초", "설탕"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00009_2.png",
+            method: "찌기",
+            category: "반찬",
+            calorie: 200,
+          },
+          {
+            id: 4,
+            recipe_name: "두부 곤약 나물 고구마 비빔밥",
+            ingredients: ["두부", "흰쌀", "현미쌀"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00010_2.png",
+            method: "끓이기",
+            category: "밥",
+            calorie: 200,
+          },
+          {
+            id: 5,
+            recipe_name: "저염 간장을 이용한 닭개장 고구마 비빔밥",
+            ingredients: ["흑미밥 쌀", "검은 쌀", "닭가슴살"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00011_2.png",
+            method: "끓이기",
+            category: "밥",
+            calorie: 200,
+          },
+        ],
+      },
+      status: {
+        code: 200,
+        message: "해당 페이지 레시피 제공에 성공하였습니다.",
+      },
+    },
+    GET_SEARCHED_RECIPES_LAST_SUCCESS: {
+      result: true,
+      content: {
+        current_page_num: 3,
+        total_page_num: 3,
+        recipes: [
+          {
+            id: 11,
+            recipe_name: "고구마 된장국",
+            ingredients: ["된장국 두부", "애느타리버섯", "감자"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00037_2.png",
+            method: "끓이기",
+            category: "국&찌개",
+            calorie: 260,
+          },
+        ],
+      },
+      status: {
+        code: 200,
+        message: "해당 페이지 레시피 제공에 성공하였습니다.",
+      },
+    },
+    // 3. get recipe detail (/api/recipe/{id})
+    GET_RECIPE_SUCCESS: {
+      result: true,
+      content: {
+        recipe: [
+          {
+            id: 11,
+            recipe_name: "고구마 된장국",
+            ingredients: ["된장국 두부", "애느타리버섯", "감자"],
+            final_img:
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00037_2.png",
+            method: "끓이기",
+            category: "국&찌개",
+            calorie: 260,
+            carbohydrates: 300,
+            proteins: 200,
+            fats: 150,
+            sodium: 30,
+            manual_desc: [
+              "1. 감자, 양파를 잘 익도록 얇게 썬다.",
+              "2. 애느타리버섯은 썰어 달궈진 팬에 굽는다.",
+              "3. 대파를 송송 썬다.",
+              "4. 냄비에 물을 붓고 된장을 푼 뒤 감 자, 양파, 두부를 넣어 재료가 투명 해지게 끓인다.",
+              "5. 된장국의 재료를 건져서 국물 한 국 자와 믹서에 넣어 갈은 다음 된장국 에 다시 넣어 한 번 더 끓인다.",
+              "6. 구운 애느타리버섯과 대파를 국에 넣어 끓인 후 그릇에 담는다.",
+            ],
+            manual_imgs: [
+              "",
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/20_00037_2.png",
+              "",
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/20_00037_4.png",
+              "",
+              "http://www.foodsafetykorea.go.kr/uploadimg/cook/20_00037_6.png	",
+            ],
+          },
+        ],
+      },
+      status: {
+        code: 200,
+        message: "레시피 세부정보 제공에 성공하였습니다.",
+      },
+    },
+    // 4. finish certain recipe (/api/recipe/finish?id={id})
+    FINISH_RECIPE_SUCCESS: {
+      result: true,
+      status: {
+        code: 200,
+        message: "해당 레시피가 요리 완료 처리되었습니다.",
+      },
+    },
+    // 5. like certain recipe (/api/recipe/like?id={id})
+    LIKE_RECIPE_SUCCESS: {
+      result: true,
+      status: {
+        code: 200,
+        message: "해당 레시피 북마크가 성공하였습니다.",
+      },
+    },
+    LIKE_RECIPE_FAIL: {
       result: false,
       status: {
         code: 400,
-        message: "해당 사용자가 입력한 식재료가 없습니다.",
+        message: "이미 북마크된 레시피입니다.",
+      },
+    },
+    // 6. unlike certain recipe (/api/recipe/unlike?id={id})
+    UNLIKE_RECIPE_SUCCESS: {
+      result: true,
+      status: {
+        code: 200,
+        message: "해당 레시피 북마크 취소가 성공하였습니다.",
+      },
+    },
+    UNLIKE_RECIPE_FAIL: {
+      result: false,
+      status: {
+        code: 400,
+        message: "이미 북마크 취소된 레시피입니다.",
       },
     },
   },
+  // statistics: 5 apis
   STATISTICS: {
     // 1. get ingredients state (/api/statistics/state)
     GET_STATE_SUCCESS: {
