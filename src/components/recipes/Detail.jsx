a0e93418a5f43b3a5e12731ab2130ff5cc848e37
ericagong <48196721+ericagong@users.noreply.github.com>
2022-09-06 10:33:33 +0900

    feat: bookmrak recipe

--- src/components/recipes/Detail.jsx
+++ src/components/recipes/Detail.jsx
@@ -1,13 +1,16 @@
 import { useState, useCallback, useEffect } from "react";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 
 // import { apis } from "../../shared/axios";
 import RESP_CHAE from "../../server/response_chae";
 import Loader from "../common/Loader";
+import SmallIconButton from "../../elements/buttons/SmallIconButton";
 
 const Detail = ({ id, recipeName }) => {
   const [loading, setLoading] = useState(true);
   const [recipe, setRecipe] = useState({});
+  const [bookmark, setBookmark] = useState(recipe.is_liked);
 
   const get_data = useCallback(async () => {
     const resp = RESP_CHAE.RECIPES.GET_RECIPE_SUCCESS;
@@ -28,7 +31,19 @@ const Detail = ({ id, recipeName }) => {
     get_data();
   }, [get_data]);
 
-  console.log(recipe);
+  const clickHandler = async () => {
+    setBookmark((prev) => !prev);
+
+    if (!bookmark) {
+      // const resp = RESP_CHAE.RECIPES.LIKE_RECIPE_SUCCESS;
+      // const resp = RESP_CHAE.RECIPES.LIKE_RECIPE_FAIL;
+      // const resp = await apis.like_recipe({ id });
+      return;
+    }
+
+    // const resp = RESP_CHAE.RECIPES.UNLIKE_RECIPE_SUCCESS;
+    // const resp = await apis.unlike_recipe({ id });
+  };
 
   const ingredients = recipe.ingredients?.join(" ");
   const instruction = recipe.manual_desc?.map((desc, idx) => (
@@ -46,6 +61,11 @@ const Detail = ({ id, recipeName }) => {
       {loading ? <Loader /> : null}
       {!loading ? (
         <StWrapper>
+          <SmallIconButton
+            icon={faBookmark}
+            onClick={clickHandler}
+            isactive={bookmark}
+          />
           <div>{ingredients}</div>
           <div>
             <div>조리방법: {recipe.method}</div>
