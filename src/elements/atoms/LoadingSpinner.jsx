3dfb08731b57039d9454c24d520f99f6a37c0efe
ericagong <48196721+ericagong@users.noreply.github.com>
2022-09-20 00:29:15 +0900

    feat: LoadingSpinner atom 추가(react-loading-spinner)

---
+++ src/elements/atoms/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+import { RotatingLines } from "react-loader-spinner";
+
+const LoadingSpinner = () => {
+  <RotatingLines
+    strokeColor='grey'
+    strokeWidth='5'
+    animationDuration='0.5'
+    width='90'
+    visible={true}
+  />;
+};
+
+export default LoadingSpinner;
