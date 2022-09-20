3f1356a97f644e704d5a3b518fbeb9af301f114a
ericagong <48196721+ericagong@users.noreply.github.com>
2022-09-20 16:43:14 +0900

    feat: 실시간 채팅 구현중

--- src/components/classes/ClassDetail.jsx
+++ src/components/classes/ClassDetail.jsx
@@ -13,106 +13,107 @@ const ClassDetail = () => {
 
   const { roomId } = useParams();
 
-  const enterHandler = useMemo(() => {
-    stompClient.current.send(
-      `/pub/chat/${roomId}`,
-      JSON.stringify({
-        type: "enter",
-        viewer_num: 0,
-        room_id: "1",
-        member_id: 1,
-        nickname: "erica",
-        profile_img:
-          "https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg",
-        message: "first check!",
-      }),
-      {
-        Authorization: "hi auth",
-      }
-    );
-    console.log("ENTER 이벤트 전송");
-  }, [roomId]);
+  useEffect(() => {
+    // 1. webSocket 클라이언트 생성
+    // wsCreate();
+    const sock = new SockJs("http://3.36.56.125/ws");
+    stompClient.current = webstomp.over(sock);
+    let subscription;
 
-  const messageHandler = useMemo(
-    (message) => {
+    // 2. webSocket 연결
+    stompClient.current.connect({ Authorization: "hi auth" }, () => {
+      console.log("연결 완료");
+      // enterHandler();
       stompClient.current.send(
         `/pub/chat/${roomId}`,
         JSON.stringify({
-          type: "message",
+          type: "enter",
           viewer_num: 0,
           room_id: "1",
           member_id: 1,
           nickname: "erica",
           profile_img:
             "https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg",
-          message: message,
+          message: "first check!",
+        }),
+        {}
+        // {
+        //   Authorization: "hi auth",
+        // }
+      );
+
+      // subscribeHandler();
+      subscription = stompClient.current.subscribe(
+        `/sub/chat/${roomId}`,
+        (frame) => {
+          console.log(frame);
+          // 이벤트별 동작 분기 필요
+          console.log("subscription working");
+          console.log("Subscription 수행 시 돌아갈 FE 코드 작성");
+          const resp = JSON.parse(frame.body);
+          const { type } = resp;
+
+          switch (type) {
+            case "enter":
+              return;
+            case "message":
+              return;
+            case "leave":
+              return;
+            default:
+              return;
+          }
+        },
+        // { Authorization: "auth hi" }
+        {}
+      );
+    });
+
+    // 3. webSocket 연결 해지
+    return () => {
+      // leaveHandler();
+      stompClient.current.send(
+        `/pub/chat/${roomId}`,
+        JSON.stringify({
+          type: "leave",
+          viewer_num: 0,
+          room_id: "1",
+          member_id: 1,
+          nickname: "erica",
+          profile_img:
+            "https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg",
+          message: "first check!",
         }),
         {
           Authorization: "hi auth",
         }
       );
-      console.log("Send Message");
-    },
-    [roomId]
-  );
+      console.log("퇴장 메시지 전송");
+      subscription.current.unsubscribe();
+      stompClient.current.disconnect();
+    };
+  }, [roomId]);
 
-  const leaveHandler = useMemo(() => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const message = e.target.querySelector("input").value;
+    // messageHandler(message);
     stompClient.current.send(
       `/pub/chat/${roomId}`,
       JSON.stringify({
-        type: "leave",
+        type: "message",
         viewer_num: 0,
         room_id: "1",
         member_id: 1,
         nickname: "erica",
         profile_img:
           "https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg",
-        message: "first check!",
+        message: message,
       }),
       {
         Authorization: "hi auth",
       }
     );
-    console.log("퇴장 메시지 전송");
-  }, [roomId]);
-
-  const subscribeHandler = useMemo(() => {
-    subscription.current = stompClient.current.subscribe(
-      `/sub/chat/${roomId}`,
-      (frame) => {
-        console.log(frame);
-        // 이벤트별 동작 분기 필요
-        console.log("subscription working");
-        console.log("Subscription 수행 시 돌아갈 FE 코드 작성");
-      },
-      { Authorization: "auth hi" }
-    );
-  }, [roomId]);
-
-  useEffect(() => {
-    // 1. webSocket 클라이언트 생성
-    const sock = new SockJs("http://3.36.56.125/ws");
-    stompClient.current = webstomp.over(sock);
-
-    // 2. webSocket 연결
-    stompClient.current.connect({ Authorization: "hi auth" }, () => {
-      console.log("연결 완료");
-      enterHandler();
-      subscribeHandler();
-    });
-
-    // 3. webSocket 연결 해지
-    return () => {
-      leaveHandler();
-      subscription.current.unsubscribe();
-      stompClient.current.disconnect();
-    };
-  }, [roomId, enterHandler, leaveHandler, subscribeHandler]);
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const message = e.target.querySelector("input").value;
-    messageHandler(message);
   };
 
   return (
