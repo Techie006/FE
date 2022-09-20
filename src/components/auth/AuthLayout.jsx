6e2c555ab1164a0f1d5df0cae4505d5f600cde14
ericagong <48196721+ericagong@users.noreply.github.com>
2022-09-20 16:44:51 +0900

    feat: auth refactoring

---
+++ src/components/auth/AuthLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormLayout from './FormLayout';
+
+
+const AuthLayout = () => {
+    return (
+        <div>AuthLayout
+            <FormLayout/>
+        </div>
+    );
+};
+
+export default AuthLayout;
---
+++ src/components/auth/FormLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ServiceInfo from './ServiceInfo';
+import SigninForm from './SigninForm';
+
+
+const FormLayout = () => {
+    return (
+        <div>
+            <ServiceInfo/>
+            <SigninForm/>
+        </div>
+    );
+};
+
+export default FormLayout;
