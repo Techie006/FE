56c80bd570fb8aefd361ea0aeb4462c9f77e0a9d
ericagong <48196721+ericagong@users.noreply.github.com>
2022-09-21 10:37:59 +0900

    refactor: userData 리듀서 auth 리듀서로 통합

--- src/modules/redux/auth.jsx
+++ src/modules/redux/auth.jsx
@@ -2,20 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   isLogin: localStorage.getItem("Authorization") ? true : false,
+  userInfo: {
+    member_id: 1,
+    nickname: "",
+    profile_img: "",
+  },
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login: (state, action) => {
+    login: (state, _) => {
       state.isLogin = true;
     },
-    logout: (state, action) => {
+    saveInfo: (state, action) => {
+      state.userData = { ...action.payload };
+    },
+    logout: (state, _) => {
       state.isLogin = false;
     },
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, saveInfo, logout } = authSlice.actions;
 export default authSlice.reducer;
--- src/modules/store/ConfigStore.jsx
+++ src/modules/store/ConfigStore.jsx
@@ -3,11 +3,10 @@ import logger from "redux-logger";
 
 import auth from "../redux/auth";
 import calendar from "../redux/calendar";
-import user from "../redux/userData";
 import cookingClass from "../redux/cookingClass";
 
 const store = configureStore({
-  reducer: { auth, calendar, user, cookingClass },
+  reducer: { auth, calendar, cookingClass },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
