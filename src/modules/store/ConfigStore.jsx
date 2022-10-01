f50b7e6df7ceea18f905807fcc522f2e38c92041
ericagong <48196721+ericagong@users.noreply.github.com>
2022-10-01 17:33:15 +0900

    fix: redux module name 수정

--- src/modules/store/ConfigStore.jsx
+++ src/modules/store/ConfigStore.jsx
@@ -3,14 +3,13 @@ import logger from "redux-logger";
 
 import auth from "../redux/auth";
 import calendar from "../redux/calendar";
-import cookingClassuser from "../redux/cookingClass";
+import cookingClass from "../redux/cookingClass";
 import user from "../redux/userData";
-// import storage from "../redux/storage";
 import searchData from "../redux/searchData";
 import recipeData from "../redux/recipeData";
 
 const store = configureStore({
-  reducer: { auth, calendar, cookingClassuser, searchData, recipeData, user },
+  reducer: { auth, calendar, cookingClass, searchData, recipeData, user },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
