15cf425d1609a1f55f73ddebbacc6ad528f7a9d3
plumpsqrl9744 <rnwkddn1234@nate.com>
2022-10-02 23:10:50 +0900

    feat: login page RWD work

--- src/components/auth/FormLayout.jsx
+++ src/components/auth/FormLayout.jsx
@@ -3,16 +3,38 @@ import GridTemplate from "../../elements/templates/GridTemplate";
 import SigninForm from './SigninForm';
 import styled from "styled-components";
 
-
-
 const FormLayout = () => {
     return (
-            <StWrapper>
-                <SigninForm/>
-            </StWrapper>
+        <div>
+            <GridTemplate></GridTemplate>
+            <SigninForm/>
+        </div>
     );
 };
 
 export default FormLayout;
 
-const StWrapper = styled.div``
+const StGrid = styled.div`
+  background: ${(props) => props.theme.section.layout.background};
+  border-radius: ${(props) => props.theme.section.layout.borderRadius};
+  box-shadow: ${(props) => props.theme.section.layout.boxShadow};
+`;
+
+const StCalendarSection = styled(StGrid)`
+  grid-column: 1 / span 8;
+
+  /* mobile */
+  @media all and (max-width: 600px) {
+    grid-column: 1 / span 4;
+  }
+`;
+
+const StWeekSection = styled(StGrid)`
+  grid-column: 9 / span 4;
+
+  /* mobile */
+  @media all and (max-width: 600px) {
+    grid-column: 1 / span 4;
+  }
+`;
+
--- src/modules/store/ConfigStore.jsx
+++ src/modules/store/ConfigStore.jsx
@@ -13,6 +13,7 @@ import recipeData from "../redux/recipeData";
 const store = configureStore({
   reducer: { auth, calendar, cookingClassuser, searchData, recipeData, user },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  
 });
 
 export default store;
