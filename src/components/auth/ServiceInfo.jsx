7fea0d763094695eca846aea8884f326cb94d5f2
plumpsqrl9744 <rnwkddn1234@nate.com>
2022-10-05 13:31:47 +0900

    feat: recipe css

--- src/components/auth/ServiceInfo.jsx
+++ src/components/auth/ServiceInfo.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import styled from "styled-components";
+// import React from 'react';
+// import styled from "styled-components";
 
-const ServiceInfo = () => {
-    return (
-        <StServiceInfoWrapper>
-            ServiceInfo
-        </StServiceInfoWrapper>
-    );
-};
+// const ServiceInfo = () => {
+//     return (
+//         <StServiceInfoWrapper>
+//             ServiceInfo
+//         </StServiceInfoWrapper>
+//     );
+// };
 
-export default ServiceInfo;
+// export default ServiceInfo;
 
-const StServiceInfoWrapper = styled.div`
-    height : 384px; 
-    background-color :black;
-    width : calc(100% - 74px);
-    margin : 20%0 35px 0px 35px;
+// const StServiceInfoWrapper = styled.div`
+//     height : 384px; 
+//     background-color :black;
+//     width : calc(100% - 74px);
+//     margin : 20%0 35px 0px 35px;
 
-`
+// `
