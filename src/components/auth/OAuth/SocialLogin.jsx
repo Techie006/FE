65eb4133723e27d94721cccc1632bfebe2758446
ericagong <48196721+ericagong@users.noreply.github.com>
2023-03-24 14:25:09 +0900

    fix: vercel 연결 테스팅 위한 코드 수정

--- src/components/auth/OAuth/SocialLogin.jsx
+++ src/components/auth/OAuth/SocialLogin.jsx
@@ -1,67 +1,68 @@
-import { useDispatch } from "react-redux";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { useEffect, useCallback } from "react";
-import Swal from "sweetalert2";
+import { useDispatch } from 'react-redux'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useEffect, useCallback } from 'react'
+import Swal from 'sweetalert2'
 
-import { apis } from "../../../shared/axios";
-import { signin } from "../../../modules/redux/auth";
+import { apis } from '../../../shared/axios'
+import { signin } from '../../../modules/redux/auth'
 
 const SocialLogin = ({ type }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   // OAuth를 수행하기 위한 code를 url로부터 파싱
-  const [searchParams] = useSearchParams();
-  const code = searchParams.get("code");
+  const [searchParams] = useSearchParams()
+  const code = searchParams.get('code')
 
   const oAuth = useCallback(async () => {
     // 소셜 로그인 수행
-    let resp = {};
-    if (type === "google") {
-      resp = await apis.sign_in_google({ code });
+    let resp = {}
+    if (type === 'google') {
+      resp = await apis.sign_in_google({ code })
+      console.log(code)
     }
-    if (type === "kakao") {
-      resp = await apis.sign_in_kakao({ code });
+    if (type === 'kakao') {
+      resp = await apis.sign_in_kakao({ code })
     }
 
-    const { authorization, refresh_token } = resp.headers;
+    const { authorization, refresh_token } = resp.headers
 
     const {
       result,
       content,
       status: { message },
-    } = resp.data;
+    } = resp.data
 
     // 이미 가입한 유저가 동일한 메일 주소로 소셜 로그인을 시도할 경우 에러 처리
     if (!result) {
       Swal.fire({
-        icon: "fail",
+        icon: 'fail',
         title: message,
         showConfirmButton: false,
         timer: 1000,
-      });
+      })
 
-      navigate("/auth");
+      navigate('/auth')
 
-      return;
+      return
     }
 
     // accessToken, refreshToken 저장
-    localStorage.setItem("Authorization", authorization);
-    localStorage.setItem("RefreshToken", refresh_token);
+    localStorage.setItem('Authorization', authorization)
+    localStorage.setItem('RefreshToken', refresh_token)
 
-    const { member_id, username, profile_img } = content;
-    localStorage.setItem("userId", member_id);
-    localStorage.setItem("username", username);
-    localStorage.setItem("profileImg", profile_img);
+    const { member_id, username, profile_img } = content
+    localStorage.setItem('userId', member_id)
+    localStorage.setItem('username', username)
+    localStorage.setItem('profileImg', profile_img)
 
     Swal.fire({
-      icon: "success",
+      icon: 'success',
       title: `${username}님 환영합니다!`,
       showConfirmButton: false,
       timer: 1000,
-    });
+    })
 
     dispatch(
       signin({
@@ -70,18 +71,18 @@ const SocialLogin = ({ type }) => {
           username: username,
           profileImg: profile_img,
         },
-      })
-    );
+      }),
+    )
 
     // 메인화면으로 이동
-    navigate("/");
-  }, [type, dispatch, navigate]);
+    navigate('/')
+  }, [type, dispatch, navigate])
 
   useEffect(() => {
-    oAuth();
-  }, [oAuth]);
+    oAuth()
+  }, [oAuth])
 
-  return <></>;
-};
+  return <></>
+}
 
-export default SocialLogin;
+export default SocialLogin
